0098b3b045ad7f10ad0a38456bc67721162786bb
zojian <hellozojian@gmail.com>
2020-01-01 23:53:15 +0800

    integrate with spring cloud gateway

--- gateway/src/main/java/com/tomatoclock/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/tomatoclock/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.tomatoclock.userserver;
+package com.tomatoclock.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserServerApplication {
+public class GatewayApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(UserServerApplication.class, args);
+    SpringApplication.run(GatewayApplication.class, args);
   }
 }
--- task-server/src/main/java/com/tomatoclock/taskserver/TaskServerApplication.java
+++ task-server/src/main/java/com/tomatoclock/taskserver/TaskServerApplication.java
@@ -2,10 +2,12 @@ package com.tomatoclock.taskserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class TaskServerApplication {
 
   public static void main(String[] args) {
